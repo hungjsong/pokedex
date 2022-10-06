fe0243d9115444d94b7b259456262cb69e393f6b
hungjs <hungjiesong@gmail.com>
2022-10-06 16:35:29 +0800

    Fix incorrect assignment of reducer value in configureStore

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import pokedexReducer from '../features/pokedex/pokedexSlice';
 
+const reducer = {
+  pokedex: pokedexReducer,
+};
+
 export default configureStore({
-  reducer: pokedexReducer,
+  reducer: reducer,
 });
