5cc0b64a6b7859ceffde68ce1866f7e2a144a6c9
hungjs <hungjiesong@gmail.com>
2022-10-06 19:27:35 +0800

    Create function to call API to fetch species details of a Pokemon, and implement reducer details to set value of state attribute speciesDetails

--- src/features/pokedex/pokedexSlice.ts
+++ src/features/pokedex/pokedexSlice.ts
@@ -4,14 +4,18 @@ export const pokedexSlice = createSlice({
   name: 'pokedex',
   initialState: {
     pokemonEntry: {},
+    speciesDetails: {},
   },
   reducers: {
     //API documentation can be found here: https://pokeapi.co/docs/v2
     setPokemonEntry: (state, action) => {
       state.pokemonEntry = action.payload;
     },
+    setSpeciesDetails: (state, action) => {
+      state.speciesDetails = action.payload;
+    },
   },
 });
 
-export const { setPokemonEntry } = pokedexSlice.actions;
+export const { setPokemonEntry, setSpeciesDetails } = pokedexSlice.actions;
 export default pokedexSlice.reducer;
--- src/pokemon.ts
+++ src/pokemon.ts
@@ -1,5 +1,28 @@
+import { capitalize } from './utilityFunctions';
+
 export const getPokedexEntry = (pokemon: String | number) => {
   return fetch('https://pokeapi.co/api/v2/pokemon/' + pokemon)
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+export const getPokemonTypes = (types: any) => {
+  if (types === undefined) {
+    return '';
+  }
+  let pokemonTypes: String[] = [];
+  types.forEach((type: any) => {
+    const capitalizedType = capitalize(type.type.name);
+    pokemonTypes.push(capitalizedType);
+  });
+
+  return pokemonTypes.join('/');
+};
+
+export const getSpeciesDetails = (pokemonID: number) => {
+  return fetch('https://pokeapi.co/api/v2/pokemon-species/' + pokemonID)
     .then((response) => response.json())
     .catch((error) => {
       console.error(error);
