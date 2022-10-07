3ec319e0eb39b9bb1d5c7f3f6e9cada71291ceb6
hungjs <hungjiesong@gmail.com>
2022-10-07 20:58:25 +0800

    Fix errors/address suggestions from ESLint

--- src/pokemon.ts
+++ src/pokemon.ts
@@ -1,7 +1,7 @@
 import { capitalize } from './utilityFunctions';
 
 //API documentation can be found here: https://pokeapi.co/docs/v2
-export const getPokedexEntry = (pokemon: String | number) => {
+export const getPokedexEntry = (pokemon: string | number) => {
   return fetch('https://pokeapi.co/api/v2/pokemon/' + pokemon)
     .then((response) => response.json())
     .catch((error) => {
@@ -13,7 +13,7 @@ export const getPokemonTypes = (types: any) => {
   if (types === undefined) {
     return '';
   }
-  let pokemonTypes: String[] = [];
+  const pokemonTypes: string[] = [];
   types.forEach((type: any) => {
     const capitalizedType = capitalize(type.type.name);
     pokemonTypes.push(capitalizedType);
@@ -22,7 +22,7 @@ export const getPokemonTypes = (types: any) => {
   return pokemonTypes.join('/');
 };
 
-export const getSpeciesDetails = (pokemonID: String | number) => {
+export const getSpeciesDetails = (pokemonID: string | number) => {
   return fetch('https://pokeapi.co/api/v2/pokemon-species/' + pokemonID)
     .then((response) => response.json())
     .catch((error) => {
--- src/utilityFunctions.ts
+++ src/utilityFunctions.ts
@@ -1,3 +1,3 @@
-export const capitalize = (word: String) => {
+export const capitalize = (word: string) => {
   return word.charAt(0).toUpperCase() + word.slice(1);
 };
