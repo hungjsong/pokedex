1af099a1a654dc9e82fb21df790f4564e5714b21
hungjs <hungjiesong@gmail.com>
2022-10-12 22:30:25 +0800

    Add type for Pokemon and move

--- src/types/pokemonTypes.ts
+++ src/types/pokemonTypes.ts
@@ -17,6 +17,48 @@ export type PokemonEntry = {
   weight: number;
 };
 
+export type Move = {
+  name: string;
+  type: PokemonType;
+  category: string;
+  accuracy: number;
+  powerPoint: number;
+  power: number;
+  additional_effect: {
+    description: string;
+    chance: number;
+  };
+};
+
+export type Pokemon = {
+  name: string;
+  id: number;
+  moves: Move[];
+  item: string;
+  level: number;
+  gender: string;
+  happiness: number;
+  shiny: boolean;
+  types: PokemonType[];
+  nature: string;
+  iv: {
+    hp: number;
+    atk: number;
+    def: number;
+    spAtk: number;
+    spDef: number;
+    speed: number;
+  };
+  ev: {
+    hp: number;
+    atk: number;
+    def: number;
+    spAtk: number;
+    spDef: number;
+    speed: number;
+  };
+};
+
 export type Genus = {
   genus: string;
   language: {
