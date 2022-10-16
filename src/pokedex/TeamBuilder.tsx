cde36287458ad7474965fd177b7e672d76b87ca8
hungjs <hungjiesong@gmail.com>
2022-10-16 19:53:44 +0800

    Remove unused imports and code and import PokemonList functional component

--- src/pokedex/TeamBuilder.tsx
+++ src/pokedex/TeamBuilder.tsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import PokemonList from './PokemonList';
 
 function TeamBuilder() {
   const { t } = useTranslation();
-  const dispatch = useDispatch();
 
   return (
     <div>
       <h1>{t('teamBuilder')}</h1>
+      <PokemonList />
       <nav>
         <Link to="/">{t('home')}</Link>
       </nav>
