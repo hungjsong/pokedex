c92aef7fed2982887592234cbc2f265aef4aa9f9
hungjs <hungjiesong@gmail.com>
2022-10-27 19:38:50 +0800

    Replace PokemonList functional component with PokemonSlot functional component. PokemonList will be returned within PokemonSlot along with other input fields

--- src/pokedex/TeamBuilder.tsx
+++ src/pokedex/TeamBuilder.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
-import PokemonList from './PokemonList';
+import PokemonSlot from './PokemonSlot';
 
 function TeamBuilder() {
   const { t } = useTranslation();
@@ -9,7 +9,12 @@ function TeamBuilder() {
   return (
     <div>
       <h1>{t('teamBuilder')}</h1>
-      <PokemonList />
+      <PokemonSlot position={0} />
+      <PokemonSlot position={1} />
+      <PokemonSlot position={2} />
+      <PokemonSlot position={3} />
+      <PokemonSlot position={4} />
+      <PokemonSlot position={5} />
       <nav>
         <Link to="/">{t('home')}</Link>
       </nav>
