41de372ed2afbe81215cbf692b6b7b13bc93a66d
hungjs <hungjiesong@gmail.com>
2022-10-29 14:56:38 +0800

    Add 6 JSON objects of type 'Pokemon' to the initialState attribute 'team'. Adjust reducers setPokemon and setNature to set the values of name and nature respectively in the index specified by action.payload.slotNumber

--- src/features/pokedex/teamBuilderSlice.ts
+++ src/features/pokedex/teamBuilderSlice.ts
@@ -1,17 +1,202 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { PokemonEntry, SpeciesDetails } from '../../types/pokemonTypes';
+import {
+  Pokemon,
+  PokemonEntry,
+  SpeciesDetails,
+} from '../../types/pokemonTypes';
 
-interface TeamBuilderState {}
+interface TeamBuilderState {
+  team: Pokemon[];
+}
 
-const initialState: TeamBuilderState = {};
+const initialState: TeamBuilderState = {
+  team: [
+    {
+      name: null,
+      id: null,
+      moves: [],
+      item: null,
+      level: 1,
+      gender: null,
+      happiness: 0,
+      shiny: false,
+      types: [],
+      nature: null,
+      iv: {
+        hp: 0,
+        atk: 0,
+        def: 0,
+        spAtk: 0,
+        spDef: 0,
+        speed: 0,
+      },
+      ev: {
+        hp: 0,
+        atk: 0,
+        def: 0,
+        spAtk: 0,
+        spDef: 0,
+        speed: 0,
+      },
+    },
+    {
+      name: null,
+      id: null,
+      moves: [],
+      item: null,
+      level: 1,
+      gender: null,
+      happiness: 0,
+      shiny: false,
+      types: [],
+      nature: null,
+      iv: {
+        hp: 0,
+        atk: 0,
+        def: 0,
+        spAtk: 0,
+        spDef: 0,
+        speed: 0,
+      },
+      ev: {
+        hp: 0,
+        atk: 0,
+        def: 0,
+        spAtk: 0,
+        spDef: 0,
+        speed: 0,
+      },
+    },
+    {
+      name: null,
+      id: null,
+      moves: [],
+      item: null,
+      level: 1,
+      gender: null,
+      happiness: 0,
+      shiny: false,
+      types: [],
+      nature: null,
+      iv: {
+        hp: 0,
+        atk: 0,
+        def: 0,
+        spAtk: 0,
+        spDef: 0,
+        speed: 0,
+      },
+      ev: {
+        hp: 0,
+        atk: 0,
+        def: 0,
+        spAtk: 0,
+        spDef: 0,
+        speed: 0,
+      },
+    },
+    {
+      name: null,
+      id: null,
+      moves: [],
+      item: null,
+      level: 1,
+      gender: null,
+      happiness: 0,
+      shiny: false,
+      types: [],
+      nature: null,
+      iv: {
+        hp: 0,
+        atk: 0,
+        def: 0,
+        spAtk: 0,
+        spDef: 0,
+        speed: 0,
+      },
+      ev: {
+        hp: 0,
+        atk: 0,
+        def: 0,
+        spAtk: 0,
+        spDef: 0,
+        speed: 0,
+      },
+    },
+    {
+      name: null,
+      id: null,
+      moves: [],
+      item: null,
+      level: 1,
+      gender: null,
+      happiness: 0,
+      shiny: false,
+      types: [],
+      nature: null,
+      iv: {
+        hp: 0,
+        atk: 0,
+        def: 0,
+        spAtk: 0,
+        spDef: 0,
+        speed: 0,
+      },
+      ev: {
+        hp: 0,
+        atk: 0,
+        def: 0,
+        spAtk: 0,
+        spDef: 0,
+        speed: 0,
+      },
+    },
+    {
+      name: null,
+      id: null,
+      moves: [],
+      item: null,
+      level: 1,
+      gender: null,
+      happiness: 0,
+      shiny: false,
+      types: [],
+      nature: null,
+      iv: {
+        hp: 0,
+        atk: 0,
+        def: 0,
+        spAtk: 0,
+        spDef: 0,
+        speed: 0,
+      },
+      ev: {
+        hp: 0,
+        atk: 0,
+        def: 0,
+        spAtk: 0,
+        spDef: 0,
+        speed: 0,
+      },
+    },
+  ],
+};
 
 export const teamBuilderSlice = createSlice({
   name: 'teamBuilder',
   initialState,
   reducers: {
-    setPokemon: (state, action) => {},
+    setPokemon: (state, action) => {
+      state.team[action.payload.slotNumber].name = action.payload.name;
+    },
+    setMove: (state, action) => {
+      console.log('hi'); //Added console log for now to prevent empty reducer error
+    },
+    setNature: (state, action) => {
+      state.team[action.payload.slotNumber].nature = action.payload.nature;
+    },
   },
 });
 
-export const { setPokemon } = teamBuilderSlice.actions;
+export const { setPokemon, setNature } = teamBuilderSlice.actions;
 export default teamBuilderSlice.reducer;
