cb54e4303b3b79daaba242abbcab992b548aecda
hungjs <hungjiesong@gmail.com>
2022-11-10 16:54:31 +0800

    Define function calculateCaptureRateGen8, which calculates the overall capture rate of a Pokemon based on several variables such as what ball was used, what is the status of the Pokemon, etc. Define functions to calculate the value of these variables (calculateBallBonus currently only returns the default value).

--- src/components/catchingSimulator/CatchingSimulator.tsx
+++ src/components/catchingSimulator/CatchingSimulator.tsx
@@ -1,11 +1,71 @@
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
 function CatchingSimulator() {
   const { t } = useTranslation();
+  const [maximumHP, setMaximumHP] = useState(100);
+  const [currentHP, setCurrentHP] = useState(1);
+  const [catchRate, setCatchRate] = useState(1);
+  const [currentLevel, setCurrentLevel] = useState(1);
+  const [statusCondition, setStatusCondition] = useState('');
+  const [numOfPokemonCaught, setNumOfPokemonCaught] = useState(0);
+  const [finalCaptureRate, setFinalCaptureRate] = useState(0);
+  const [ballUsed, setBallUsed] = useState(0);
+  const [storyCompleted, setStoryCompleted] = useState(false);
+
+  useEffect(() => {
+    calculateCaptureRateGen8();
+  }, []);
+
+  function calculateBallBonus() {
+    return 1; //WIP
+  }
+
+  function calculateStatusConditionModifier() {
+    if (statusCondition === 'asleep' || statusCondition === 'frozen') {
+      return 2.5;
+    } else if (
+      statusCondition === 'poisoned' ||
+      statusCondition === 'paralyzed' ||
+      statusCondition === 'burned'
+    ) {
+      return 1.5;
+    }
+    return 1;
+  }
+
+  function calculateDifficultyModifier() {
+    if (!storyCompleted) {
+      return 410 / 4096;
+    } else {
+      return 1;
+    }
+  }
+
+  function calculateCaptureRateGen8() {
+    const grassModifier = 1;
+    const ballBonus = calculateBallBonus();
+    const statusConditionModifier = calculateStatusConditionModifier();
+    const lowLevelModifier = currentLevel < 20 ? (30 - currentLevel) / 10 : 1;
+    const difficultyModifier = calculateDifficultyModifier();
+    const finalCaptureRate =
+      (((3 * maximumHP - 2 * currentHP) *
+        grassModifier *
+        catchRate *
+        ballBonus) /
+        (3 * maximumHP)) *
+      lowLevelModifier *
+      statusConditionModifier *
+      difficultyModifier;
+
+    setFinalCaptureRate(+(finalCaptureRate * 100).toFixed(2));
+  }
 
   return (
     <nav>
+      <h1>Catching Simulator</h1>
+      <p>{finalCaptureRate}%</p>
       <Link to="/">{t('home')}</Link>
     </nav>
   );
