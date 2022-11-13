c5c501014ba40dd019cbca14c678cd1f3627ed4a
hungjs <hungjiesong@gmail.com>
2022-11-13 22:01:15 +0800

    Create redux slice catchingSimulatorSlice with basic structure, and add slice to the redux store.

---
+++ src/redux/catchingSimulatorSlice.ts
@@ -0,0 +1,72 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { Pokemon } from '../types/pokemonTypes';
+
+interface CatchingSimulatorState {
+  pokemon: Pokemon;
+}
+
+const initialState: CatchingSimulatorState = {
+  pokemon: {
+    name: 'Bulbasaur',
+    id: 1,
+    level: 1,
+    gender: 'Male',
+    shiny: false,
+    types: [
+      {
+        slot: 0,
+        type: {
+          name: 'grass',
+          url: '',
+        },
+      },
+      {
+        slot: 1,
+        type: {
+          name: 'poison',
+          url: '',
+        },
+      },
+    ],
+    nature: { name: 'docile', increased_stat: null, decreased_stat: null },
+    baseStats: {
+      hp: 45,
+      atk: 49,
+      def: 49,
+      spAtk: 65,
+      spDef: 65,
+      spd: 45,
+    },
+    iv: {
+      hp: 31,
+      atk: 31,
+      def: 31,
+      spAtk: 31,
+      spDef: 31,
+      spd: 31,
+    },
+    ev: {
+      hp: 0,
+      atk: 0,
+      def: 0,
+      spAtk: 0,
+      spDef: 0,
+      spd: 0,
+    },
+    catchRate: 45,
+    weight: 6.9,
+  },
+};
+
+export const catchingSimulatorSlice = createSlice({
+  name: 'catchingSimulator',
+  initialState,
+  reducers: {
+    setPokemon: (state, action) => {
+      state.pokemon = action.payload;
+    },
+  },
+});
+
+export const {} = catchingSimulatorSlice.actions;
+export default catchingSimulatorSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import pokedexReducer from './pokedexSlice';
 import teamBuilderReducer from './teamBuilderSlice';
+import catchingSimulatorReducer from './catchingSimulatorSlice';
 
 const reducer = {
   pokedex: pokedexReducer,
   teamBuilder: teamBuilderReducer,
+  catchingSimulator: catchingSimulatorReducer,
 };
 
 const store = configureStore({
